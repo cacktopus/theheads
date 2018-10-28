499a68cfe747fddc7f8a3ba050adafb99020c51d
Jeremy Suurkivi <suurkivi@gmail.com>
2018-10-28 15:25:39 -0700

    initial

---
+++ head.py
@@ -0,0 +1,83 @@
+import base64
+import os
+import random
+import sys
+import time
+
+import etcd3
+
+from multiprocessing import Pool, Lock
+
+from etcd.etcdserver.api.v3lock.v3lockpb.v3lock_pb2 import LockResponse
+
+stdout_lock = Lock()
+
+hosts = [
+    "192.168.1.10",
+    "192.168.1.11",
+    "192.168.1.12",
+]
+
+
+def get_lock(args):
+    LockResponse
+
+    pid, lockname, t0 = args
+
+    def out(*args, **kw):
+        t = time.time() - t0
+        with stdout_lock:
+            print("{} | {:>6.2f} |".format(pid, t), *args, **kw)
+            sys.stdout.flush()
+
+    host = random.choice(hosts)
+    out("start", host)
+
+    conn = etcd3.Etcd3Client(host=host)
+
+    out("connected")
+
+    next_tick = 15 - (time.time() % 15)
+    out("delay {:.2f}".format(next_tick))
+
+    time.sleep(next_tick)
+
+    out("calling lock")
+    lock = conn.lock(lockname, ttl=10)
+
+    out("lock: acquired={}, name={}, key={}, uuid={}, ttl={}".format(
+        lock.is_acquired(),
+        lock.name,
+        lock.key,
+        base64.b64encode(lock.uuid),
+        lock.ttl,
+    ))
+
+    out("acquiring")
+    acquired = lock.acquire()
+
+    out(" - I GOT THE LOCK - " if acquired else "I didn't get the lock")
+
+    out("lock: acquired={}, name={}, key={}, uuid={}, ttl={}".format(
+        lock.is_acquired(),
+        lock.name,
+        lock.key,
+        base64.b64encode(lock.uuid),
+        lock.ttl,
+    ))
+
+    out("sleeping")
+    time.sleep(120)
+
+
+def main():
+    pid = os.getpid()
+    lockname = "lock-{}".format(pid)
+    t0 = time.time()
+
+    with Pool(5) as p:
+        p.map(get_lock, [(i, lockname, t0) for i in range(5)])
+
+
+if __name__ == '__main__':
+    main()
---
+++ main.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 320)
+cap.set(4, 240)
+
+while True:
+
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (9, 9), 0)
+
+    text = "abc123"
+    cv2.putText(gray, "Room Status: {}".format(text), (10, 20),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+    # Display the resulting frame
+    cv2.imshow('frame', gray)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
