1b9a01328f10638659c3d0bfab38c6c483ccf142
Jeremy Suurkivi <suurkivi@gmail.com>
2018-11-01 12:51:52 -0700

    All working

--- main.py
+++ main.py
@@ -2,111 +2,133 @@ import time
 
 import cv2
 import requests
-
-cap = cv2.VideoCapture(0)
-# cap.set(3, 320)
-# cap.set(4, 240)
+import threading
+import queue
 
 MIN_AREA = 500 * 4
 NUM_FRAMES = 10
-frame_times = []
 
-SCALE = 25
+SCALE = 22.5
 
-avg = None
+q = queue.Queue()
 
-# warmup
-for i in range(5):
-    ret, frame = cap.read()
 
-do_dilate = True
+def motion_detect(cap):
+    # cap.set(3, 320)
+    # cap.set(4, 240)
 
-while True:
-    fps = 0
-    ret, frame = cap.read()
-    frame = cv2.flip(frame, 1)
-    height, width, channels = frame.shape
+    frame_times = []
+    avg = None
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # gray = frame
-    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+    # warmup
+    for i in range(5):
+        ret, frame = cap.read()
 
-    if avg is None:
-        avg = gray.copy().astype("float32")
-        continue
+    do_dilate = True
 
-    cv2.accumulateWeighted(gray, avg, 0.4)
+    while True:
+        fps = 0
+        ret, frame = cap.read()
+        frame = cv2.flip(frame, 1)
+        height, width, channels = frame.shape
 
-    frame_delta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
-    thresh = cv2.threshold(frame_delta, 20, 255, cv2.THRESH_BINARY)[1]
+        # Our operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # gray = frame
+        gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-    if do_dilate:
-        thresh = cv2.dilate(thresh, None, iterations=2)
+        if avg is None:
+            avg = gray.copy().astype("float32")
+            continue
 
-    _, countours, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-                                       cv2.CHAIN_APPROX_SIMPLE)
+        cv2.accumulateWeighted(gray, avg, 0.4)
 
-    countours = [c for c in countours if cv2.contourArea(c) > MIN_AREA]
+        frame_delta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
+        thresh = cv2.threshold(frame_delta, 20, 255, cv2.THRESH_BINARY)[1]
 
-    if countours:
-        _, track = max((cv2.contourArea(c), c) for c in countours)
-    else:
-        track = None
+        if do_dilate:
+            thresh = cv2.dilate(thresh, None, iterations=2)
 
-    ############################ draw `show` ##############################
-    # show = avg.astype("uint8")
-    show = frame
+        _, countours, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                                           cv2.CHAIN_APPROX_SIMPLE)
 
-    if track is not None:
-        (x, y, w, h) = cv2.boundingRect(track)
-        cv2.rectangle(show, (x, y), (x + w, y + h), (128, 128, 128), 2)
+        countours = [c for c in countours if cv2.contourArea(c) > MIN_AREA]
 
-        pos = x + w // 2
-        cv2.line(show, (pos, y), (pos, y + h), (128, 128, 128), 2)
+        if countours:
+            _, track = max((cv2.contourArea(c), c) for c in countours)
+        else:
+            track = None
 
-        half = width / 2
-        pos2 = int(SCALE * ((pos - half) / half))
-        requests.get(
-            url="http://rpi3-04:8080/position/{}?speed=1000".format(pos2)
+        ############################ draw `show` ##############################
+        # show = avg.astype("uint8")
+        show = frame
+
+        if track is not None:
+            (x, y, w, h) = cv2.boundingRect(track)
+            cv2.rectangle(show, (x, y), (x + w, y + h), (128, 128, 128), 2)
+
+            pos = x + w // 2
+            cv2.line(show, (pos, y), (pos, y + h), (128, 128, 128), 2)
+
+            half = width / 2
+            pos2 = int(SCALE * ((pos - half) / half))
+            q.put(pos2)
+
+        else:
+            pos = None
+            pos2 = None
+
+        if len(frame_times) > NUM_FRAMES:
+            frames = frame_times[-NUM_FRAMES:]
+            fps = (len(frames) - 1) / (frames[-1] - frames[0])
+
+        text = "FPS: [{:.1f}] Dilate: [{}] Contours: [{}], Pos: [{}], Pos2: [{}], Width: [{}]".format(
+            fps,
+            int(do_dilate),
+            len(countours),
+            pos,
+            pos2,
+            width,
         )
 
-    else:
-        pos = None
-        pos2 = None
+        cv2.putText(show, text, (11, 21),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
 
-    if len(frame_times) > NUM_FRAMES:
-        frames = frame_times[-NUM_FRAMES:]
-        fps = (len(frames) - 1) / (frames[-1] - frames[0])
+        cv2.putText(show, text, (10, 20),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+        # Display the resulting frame
+        cv2.imshow('frame', show)
+
+        key = cv2.waitKey(1) & 0xFF
+        if key in (ord('q'), 27):
+            break
+
+        if key == ord('d'):
+            do_dilate = not do_dilate
+
+        frame_times.append(time.time())
 
 
-    text = "FPS: [{:.1f}] Dilate: [{}] Contours: [{}], Pos: [{}], Pos2: [{}], Width: [{}]".format(
-        fps,
-        int(do_dilate),
-        len(countours),
-        pos,
-        pos2,
-        width,
-    )
+def position_head():
+    while True:
+        pos = q.get()
+        requests.get(
+            url="http://rpi3-04:8080/position/{}?speed=1000".format(pos)
+        )
 
-    cv2.putText(show, text, (11, 21),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
 
-    cv2.putText(show, text, (10, 20),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+def main():
+    t = threading.Thread(target=position_head, daemon=True)
+    t.start()
 
-    # Display the resulting frame
-    cv2.imshow('frame', show)
+    cap = cv2.VideoCapture(0)
 
-    key = cv2.waitKey(1) & 0xFF
-    if key in (ord('q'), 27):
-        break
+    motion_detect(cap)
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
 
-    if key == ord('d'):
-        do_dilate = not do_dilate
 
-    frame_times.append(time.time())
-
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    main()
