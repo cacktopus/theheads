d4ffa49442aa27e93c384a64cba18d1abb48d466
Jeremy Suurkivi <suurkivi@gmail.com>
2018-11-01 10:24:51 -0700

    dilate

--- main.py
+++ main.py
@@ -15,12 +15,15 @@ first_frame = None
 for i in range(25):
     ret, frame = cap.read()
 
+do_dilate = True
+
 while True:
     fps = 0
     ret, frame = cap.read()
 
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # gray = frame
     gray = cv2.GaussianBlur(gray, (31, 31), 0)
     gray = cv2.flip(gray, 1)
 
@@ -31,22 +34,32 @@ while True:
     frame_delta = cv2.absdiff(first_frame, gray)
     thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)[1]
 
+    if do_dilate:
+        thresh = cv2.dilate(thresh, None, iterations=2)
+
     show = thresh
 
     # draw `show`
-
     if len(frame_times) > NUM_FRAMES:
         frames = frame_times[-NUM_FRAMES:]
         fps = (len(frames) - 1) / (frames[-1] - frames[0])
 
-    cv2.putText(show, "FPS: {:.1f}".format(fps), (10, 20),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    text = "FPS: [{:.1f}] Dilate: [{}]".format(fps, int(do_dilate))
+    cv2.putText(show, text, (11, 21),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+
+    cv2.putText(show, text, (10, 20),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
     # Display the resulting frame
     cv2.imshow('frame', show)
 
-    if cv2.waitKey(1) & 0xFF in (ord('q'), 27):
+    key = cv2.waitKey(1) & 0xFF
+    if key in (ord('q'), 27):
         break
+
+    if key == ord('d'):
+        do_dilate = not do_dilate
 
     frame_times.append(time.time())
 
