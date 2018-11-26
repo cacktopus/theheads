ae1cabd3ce22c6c113fb6ade06fc1dcae75719e6
Jeremy Suurkivi <suurkivi@gmail.com>
2018-11-26 12:56:56 -0800

    Metrics

--- boss.py
+++ boss.py
@@ -1,3 +1,4 @@
+import prometheus_client
 from aiohttp import web
 
 
@@ -7,12 +8,21 @@ async def handle(request):
     return web.Response(text=text)
 
 
+async def handle_metrics(request):
+    resp = web.Response(body=prometheus_client.generate_latest())
+    resp.content_type = prometheus_client.CONTENT_TYPE_LATEST
+    return resp
+
+
 def main():
     app = web.Application()
+
     app.add_routes([
         web.get('/', handle),
+        web.get('/metrics', handle_metrics),
         web.get('/{name}', handle),
     ])
+
     web.run_app(app)
 
 
