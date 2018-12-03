7c67fce99c0331923bf8a353e64b1486f528b61c
Jeremy Suurkivi <suurkivi@gmail.com>
2018-12-03 11:30:10 -0800

    100% size

--- installation.js
+++ installation.js
@@ -1,5 +1,10 @@
 function main() {
     console.log("here we are");
     var draw = SVG('drawing').size('100%', '100%');
-    var rect = draw.rect(100, 100);
+    var rect = draw.rect(100, 300);
+    rect.width(400);
+    rect.height(300);
+    rect.x(200);
+    rect.y(50);
+    rect.height(300);
 }
