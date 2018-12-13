b4646f41d28e39df95c1eb4aa1171d0124ca2f0e
Jeremy Suurkivi <suurkivi@gmail.com>
2018-12-13 11:22:20 -0800

    Add web component

--- home.py
+++ home.py
@@ -1,14 +1,16 @@
 import json
-from typing import Tuple
+from typing import Tuple, Dict
 
 import aiohttp
 import argparse
 import asyncio
 
+from aiohttp import web
+
 CONSUL_PORT = 8500
 
 
-async def get(url: str) -> Tuple[int, str]:
+async def get(url: str) -> Tuple[int, Dict]:
     async with aiohttp.ClientSession() as session:
         async with session.get(url=url) as response:
             resp = await response.text()
@@ -20,8 +22,8 @@ async def get_nodes_for_service(consul_host: str, service: str):
     url = "http://{}:{}{}".format(consul_host, CONSUL_PORT, path)
 
     status, resp = await get(url)
-    for node in resp:
-        print("{ID}, {Node}, {Address}, {ServiceName}:{ServicePort}".format(**node))
+    assert status == 200
+    return resp
 
 
 async def get_services(consul_host: str):
@@ -32,10 +34,26 @@ async def get_services(consul_host: str):
 
     print(url)
 
+    result = {}
+
     status, resp = await get(url)
     for name, tags in resp.items():
         print(name, tags)
-        await get_nodes_for_service(consul_host, name)
+        nodes = await get_nodes_for_service(consul_host, name)
+        for node in nodes:
+            print("{ID}, {Node}, {Address}, {ServiceName}:{ServicePort}".format(**node))
+        result[name] = dict(
+            tags=tags,
+            nodes=nodes,
+        )
+    return result
+
+
+async def handle(request):
+    consul_host = request.app['consul_host']
+    services = await(get_services(consul_host))
+    text = json.dumps(services, indent=4)
+    return web.Response(text=text)
 
 
 def main():
@@ -46,8 +64,15 @@ def main():
 
     args = parser.parse_args()
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(get_services(args.consul))
+    app = web.Application()
+
+    app['consul_host'] = args.consul
+
+    app.add_routes([
+        web.get('/', handle),
+    ])
+
+    web.run_app(app, port=8082)
 
 
 if __name__ == '__main__':
