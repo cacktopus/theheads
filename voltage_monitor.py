53e67ba44fdf7614faaa468dce8fb71034063a77
Jeremy Suurkivi <suurkivi@gmail.com>
2018-12-16 19:09:58 -0800

    New voltage monitor

--- voltage_monitor.py
+++ voltage_monitor.py
@@ -5,11 +5,12 @@ from collections import deque
 from typing import Callable
 
 ROOT = "/sys/class/gpio"
+LED = "/sys/devices/platform/leds/leds/led1"
 DELAY = 1 / 25
 ACCUMULATION_WINDOW = 60  # s
 
 
-async def monitor_voltage(callback: Callable):
+async def monitor_voltage_old(callback: Callable):
     gpio = os.path.join(ROOT, "gpio35")
     if not os.path.exists(gpio):
         with open(os.path.join(ROOT, "export"), "wb") as fp:
@@ -29,6 +30,22 @@ async def monitor_voltage(callback: Callable):
 
         values.append(value)
         low_voltage_detected = any(x != 1 for x in values)
+        callback(low_voltage_detected)
+
+        await asyncio.sleep(DELAY)
+
+
+async def monitor_voltage(callback: Callable):
+    size = int(math.ceil(ACCUMULATION_WINDOW / DELAY))
+    values = deque(maxlen=size)
+
+    while True:
+        with open(LED) as fp:
+            contents = fp.read()
+        value = int(contents)
+
+        values.append(value)
+        low_voltage_detected = any(x < 255 for x in values)
         callback(low_voltage_detected)
 
         await asyncio.sleep(DELAY)
