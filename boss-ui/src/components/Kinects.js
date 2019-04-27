db3470b36d310ffb9684b0cef14f4ab71cd55195
aidanj1 <adamsjenkins@gmail.com>
2019-04-26 23:59:35 -0400

    updated

--- boss-ui/src/components/Kinects.js
+++ boss-ui/src/components/Kinects.js
@@ -12,17 +12,25 @@ export default class Menu extends React.Component {
     }
 
     render() {
-        const kinects = this.props.kinects || [];
+        const kinects = this.props.kinects;
+        console.log('render <Kinects');
 
-        if (typeof window !== 'undefined') {
-            console.log('c__kinects');
-            window.c__kinects2 = kinects;
-        }
-
-        return (
-            <div className="Kinects">
+        if (!kinects || kinects.size === 0) {
+            if (typeof window !== 'undefined') {
+                console.log('c__kinectno');
+                window.c__kinectno = kinects;
+            }
+            console.log('Kinects no kinects');
+            return <div className="Kinects Kinects--none"></div>
+        } else {
+            if (typeof window !== 'undefined') {
+                console.log('c__kinectyes');
+                window.c__kinectyes = kinects;
+            }
+            console.log('Kinects doing it');
+            return <div className="Kinects">
                 {kinects.map((kinect, i) => kinect ? <Kinect key={i} kinect={kinect} kinectIndex={i} standIndex={this.props.standIndex}/> : null)}
             </div>
-        );
+        }
     }
 }
