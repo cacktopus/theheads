aa4ab130af02df6257980421043b381a91a61e43
Jeremy Suurkivi <suurkivi@gmail.com>
2019-05-15 18:42:32 -0700

    Add node_modules2

--- services/voices/build.sh
+++ services/voices/build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 PATH=$PATH:/home/build/builds/node/prod/bin
 cd voices
-# ln -s /home/build/builds/node_modules/prod/boss-ui/node_modules
-npm install
+ln -s /home/build/builds/node_modules2/prod/voices/node_modules
