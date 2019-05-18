e36d4043f8a2a5f33a87882700b68403f31dd780
Jeremy Suurkivi <suurkivi@gmail.com>
2019-05-18 09:48:28 -0700

    Don't curl

--- services/boss/build.sh
+++ services/boss/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 ln -s ~build/builds/python-env/prod/env env
-curl -O https://cdnjs.cloudflare.com/ajax/libs/svg.js/2.7.1/svg.js
+# curl -O https://cdnjs.cloudflare.com/ajax/libs/svg.js/2.7.1/svg.js
 
 ln -s ~build/builds/boss-ui/prod/boss-ui/build boss-ui
