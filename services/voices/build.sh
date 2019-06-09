c479210f96bd231363812a008231518ea279a186
Jeremy Suurkivi <suurkivi@gmail.com>
2019-06-09 15:27:35 -0700

    Switch to python

--- services/voices/build.sh
+++ services/voices/build.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-PATH=$PATH:/home/build/builds/node/prod/bin
-cd voices
-ln -s /home/build/builds/node_modules2/prod/voices/node_modules
+ln -s ~build/builds/python-env/prod/env env
