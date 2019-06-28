c871c98ad19994b738a822e81eb4e4aeb628c41c
Jeremy Suurkivi <suurkivi@gmail.com>
2019-06-28 09:09:40 -0700

    Use 3d noise so patterns are repeating

--- walls/fun_circles.py
+++ walls/fun_circles.py
@@ -1,3 +1,4 @@
+import math
 import random
 from typing import List
 
@@ -29,22 +30,25 @@ def fun_circles(cfg):
     name = "fun-circles"
     debug_svg = svgwrite.Drawing(f'{name}.svg', profile='tiny')
     WIDTH = 146
-    NUM_WALLS = 2
+    NUM_WALLS = 8
+    PAD = 10
 
     points = poisson_disc_samples(
-        width=cfg.width + 50 + (WIDTH + 10) * NUM_WALLS,
+        width=cfg.width + 50 + (WIDTH + PAD) * NUM_WALLS,
         height=cfg.height + 50,
         r=cfg.r
     )
 
+    total_x = NUM_WALLS * (WIDTH + PAD)
+
     for i in range(NUM_WALLS):
-        x_offset = 25 + i * (WIDTH + 10)
+        x_offset = 25 + i * (WIDTH + PAD)
         selected = [p for p in points if x_offset - 25 < p[0] < x_offset + WIDTH + 25]
-        make_wall(cfg, selected, f"{name}-{i}", debug_svg, x_offset)
+        make_wall(cfg, selected, f"{name}-{i}", debug_svg, x_offset, total_x)
         debug_svg.save()
 
 
-def make_wall(cfg, points, name, debug_svg, x_offset):
+def make_wall(cfg, points, name, debug_svg, x_offset, total_x):
     wall = Wall(name, cfg, x_offset=x_offset, y_offset=25)
 
     radii = {}
@@ -54,18 +58,27 @@ def make_wall(cfg, points, name, debug_svg, x_offset):
 
     cut = Polygon.Polygon()
 
-    def map_angle(x: float, y: float) -> float:
+    def map_angle(x_in: float, y_in: float) -> float:
         a0 = 15
         f0 = 0.009
 
+        c = total_x
+
+        phi = x_in / c * 2 * math.pi
+        r = c / (2 * math.pi)
+
+        x = r * math.cos(phi)
+        y = r * math.cos(phi)
+        z = y_in
+
         return a0 * noise.snoise3(
             f0 * x,
-            0,
             f0 * y,
+            f0 * z,
         ) + 0.25 * a0 * noise.snoise3(
             2 * f0 * x + 1000,
-            0,
             2 * f0 * y + 1000,
+            2 * f0 * z + 1000,
         ) + 0.0 * a0 * (random.random() - 0.5)
 
     for i, point in enumerate(points):
