a11d9f93c7a0bb5b24859f42894f92d1dd8fccb2
Jeremy Suurkivi <suurkivi@gmail.com>
2019-07-11 08:57:09 -0700

    no numpy wheel

--- services/python-env/build.sh
+++ services/python-env/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export LD_LIBRARY_PATH=/home/build/builds/openssl/prod/lib
 python3 -m virtualenv -p /home/build/builds/python37/prod/bin/python3 env
-env/bin/pip3 install -r requirements.txt
+env/bin/pip3 install --no-binary numpy -r requirements.txt
