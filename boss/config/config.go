f5306f19fe190bfc8abdcb9acebe646af0b0f169
Jeremy Suurkivi <suurkivi@gmail.com>
2019-07-13 12:57:04 -0700

    Starting consul config for go-boss

---
+++ boss/config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"fmt"
+	"github.com/hashicorp/consul/api"
+)
+
+type Config struct {
+	RedisServer string
+	CameraName  string
+}
+
+func NewClient() *api.Client {
+	config := api.DefaultConfig()
+	config.Address = "127.0.0.1:8500"
+	client, err := api.NewClient(config)
+	if err != nil {
+		panic(err)
+	}
+	return client
+}
+
+func AllServiceURLs(client *api.Client, serviceName, tag, prefix, postfix string) (result []string, err error) {
+	services, _, err := client.Catalog().Service(serviceName, tag, &api.QueryOptions{})
+	if err != nil {
+		return
+	}
+
+	for _, s := range services {
+		result = append(result, fmt.Sprintf(
+			"%s%s:%d%s",
+			prefix,
+			s.ServiceAddress,
+			s.ServicePort,
+			postfix,
+		))
+	}
+
+	return
+}
--- boss/go.mod
+++ boss/go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/gin-gonic/gin v1.3.0
 	github.com/gomodule/redigo v1.7.0
 	github.com/gorilla/websocket v1.4.0
+	github.com/hashicorp/consul/api v1.1.0
 	github.com/mattn/go-isatty v0.0.7 // indirect
 	github.com/prometheus/client_golang v1.0.0
 	github.com/prometheus/common v0.6.0
