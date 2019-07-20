62866a7a64380a6e12edb88ef8f03d9cad922d34
Jeremy Suurkivi <suurkivi@gmail.com>
2019-07-19 22:01:10 -0700

    change build script to build go-boss

--- services/boss/build.sh
+++ services/boss/build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-ln -s ~build/builds/python-env/prod/env env
-# curl -O https://cdnjs.cloudflare.com/ajax/libs/svg.js/2.7.1/svg.js
-
+cd boss
+GO=~build/builds/go/prod/bin/go
+${GO} build
 ln -s ~build/builds/boss-ui/prod/boss-ui/build boss-ui
-
-gcc -O3 -shared trace.c -o trace.so
